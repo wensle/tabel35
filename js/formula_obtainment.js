f81f7ed4bab4ab9b8c24c27d3af191c01b6478d2
wensle <wensley.podrono@gmail.com>
2013-08-30 00:52:06 +0200

    MathJax formule wordt nu opgeslagen in json var
    * De MathJax formule wordt nu opgeslagen bij de bijbehorende property van de
    JSON variabele, dus in json.formules.[index].formule. Dit is handig, want
    zo kan ik chaching implementeren. Eerst werd elke MathJax formule niet
    opgeslagen, maar werd hij meteen geappend to the body.
    * index.php is hernoemd naar index.html
    
    Please enter the commit message for your changes. Lines starting

--- js/formula_obtainment.js
+++ js/formula_obtainment.js
@@ -5,26 +5,34 @@ function getFormula(grootheidSymbool){
 	};
 	$.getJSON("getFormula.php", data, function(json){
 		console.log("getFormula.php is gelukt!");
-		var arrFormule = "";
-			$.each(json.formules, function(index, value){
-				arrFormule = value.formule.split(",");
-				var arr = [], tony = "";
-				$.each(arrFormule, function(index, value){
+		var arrFormulePieces = "";
+			$.each(json.formules, function(indexFormule, klop){
+				arrFormulePieces = klop.formule.split(",");
+				var arr = [], elementID = "";
+				$.each(arrFormulePieces, function(index, value){
 					if ($.inArray(value, arrGrootheidSybool) > -1){
-						tony = "\\href{javascript:getFormula(\"\'" + value +"\'\")}" + "{" + value + "}";
-						arr.push(tony);}
+						elementID = "\\href{javascript:getFormula(\"\'" + value +"\'\")}" + "{" + value + "}";
+						arr.push(elementID);}
+					else if ($.inArray(value, arrGrootheidSybool) === -1){
+						arr.push(value);
+						json.formules[indexFormule].formule = arr.join("");
+					}
 					else{
 						arr.push(value);}
 				});
+				// var pl = bla();
+				// console.log(pl);
 				$( '<p>', {
-				  html: '$$' + json.formules[1].symbool + '= ' + arr.join("") + '$$',
-				  'class': 'formula'
+					html: '$$' + json.formules[1].symbool + '= ' + arr.join("") + '$$',
+					'class': 'formula'
 				}).appendTo( "body" );
 				MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+				console.log(json.formules[indexFormule].formule);
 			});
+			console.log(json.formules);
 	})
-	.fail(function() { console.log("FAIL: " + grootheidSymbool); })
-};
+	.fail(function() { console.log("FAIL: " + grootheidSymbool); });
+}
 $(document).ready(function() {
 		$.getJSON('getArrayGrootheidSymbool.php', function(json){
 			console.log("AJAX: functiesJSON.php is gelukt");
