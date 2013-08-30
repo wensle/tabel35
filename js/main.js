91cc0dd940202a4d200a93bde357acfa9de65c20
wensle <wensley.podrono@gmail.com>
2013-08-31 00:17:00 +0200

    svg toegevoegd + bijbehorende css + h1
    * svg is met d3 geappend, want dan kan je het g element er aan toevoegen.
      dit element heb je nodig. Deze svg is waar de tree van formules staks in
      komt te staan. In deze commit heb ik dus een begin gemaakt aan de tree.
    
    * De svg heb ik ook meteen gestyled.
    
    * Heb het document de titel Natuurkundeformules gegeven.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+var margin = {top: 20, right: 120, bottom: 20, left: 120},
+	width = 960 - margin.right - margin.left	,
+	height = 800 - margin.top - margin.	bottom;
 
+var tree = d3.layout.tree()
+	.size([height, width]);
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.right + margin.left)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
